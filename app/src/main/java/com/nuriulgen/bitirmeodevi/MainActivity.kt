9507d4033f6dcdaa187ea834755e921aeaf56b06
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-09-29 02:57:10 +0300

    added bottom navigation bar

--- app/src/main/java/com/nuriulgen/bitirmeodevi/MainActivity.kt
+++ app/src/main/java/com/nuriulgen/bitirmeodevi/MainActivity.kt
@@ -2,10 +2,33 @@ package com.nuriulgen.bitirmeodevi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nuriulgen.bitirmeodevi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        showBottomNavigation()
+    }
+
+    private fun showBottomNavigation(){
+        /**
+         * Nav host Fragment ile Bottom Navigation Bar birbirine bağlandı.
+         *
+         */
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
