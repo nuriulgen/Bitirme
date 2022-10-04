4c362acc2a7a6d3940e783a45be625bd5e4316ab
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-10-05 02:21:58 +0300

    Created NetworkModule class.

---
+++ app/src/main/java/com/nuriulgen/bitirmeodevi/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.nuriulgen.bitirmeodevi.di
+
+import com.nuriulgen.bitirmeodevi.data.remote.ApiService
+import com.nuriulgen.bitirmeodevi.di.repository.TopDestinationRepositoryImp
+import com.nuriulgen.bitirmeodevi.domain.repository.TopDestinationRepository
+import com.nuriulgen.bitirmeodevi.util.ProjectConstants.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideGsonConvertorFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gsonConverterFactory: GsonConverterFactory): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideService(retrofit: Retrofit):ApiService{
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTopDestinationRepository(apiService: ApiService): TopDestinationRepository{
+        return TopDestinationRepositoryImp(apiService)
+    }
+
+}
