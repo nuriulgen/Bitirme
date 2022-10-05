0f4c88c216be94903f258e448914ac0cf878297a
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-10-06 01:24:12 +0300

    Fragment scroll issue resolved.  #1 closes

--- app/src/main/java/com/nuriulgen/bitirmeodevi/adapter/NearbyAttractionsAdapter.kt
+++ app/src/main/java/com/nuriulgen/bitirmeodevi/adapter/NearbyAttractionsAdapter.kt
@@ -6,7 +6,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.nuriulgen.bitirmeodevi.R
 import com.nuriulgen.bitirmeodevi.domain.model.nearbyAttractions.NearbyAttractionsModel
+import com.nuriulgen.bitirmeodevi.util.showImage
+import com.nuriulgen.bitirmeodevi.util.showPlaceHolder
+import kotlinx.android.synthetic.main.nearby_attractions_recycler_row.view.*
 import kotlinx.android.synthetic.main.top_destination_recycler_row.view.*
+import kotlinx.android.synthetic.main.top_destination_recycler_row.view.subTitleText
+import kotlinx.android.synthetic.main.top_destination_recycler_row.view.titleText
 
 class NearbyAttractionsAdapter(private val itemList: List<NearbyAttractionsModel>): RecyclerView.Adapter<NearbyAttractionsAdapter.NearbyAttractionsViewHolder>() {
 
@@ -21,6 +26,11 @@ class NearbyAttractionsAdapter(private val itemList: List<NearbyAttractionsModel
     override fun onBindViewHolder(holder: NearbyAttractionsViewHolder, position: Int) {
         holder.itemView.titleText.text = itemList[position].city
         holder.itemView.subTitleText.text = itemList[position].country
+        holder.itemView.categoryButton.text = itemList[position].category
+
+        itemList[position].images?.get(0)?.let { holder.itemView.backgroundImage.showImage(it.url,
+            showPlaceHolder(holder.itemView.context)
+        ) }
     }
 
     override fun getItemCount(): Int {
