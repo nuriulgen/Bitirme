b4a2ac1634cec79e5b78bae1241d42ae1148b590
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-10-06 00:16:14 +0300

    created showImage extensions.

---
+++ app/src/main/java/com/nuriulgen/bitirmeodevi/util/Extensions.kt
@@ -0,0 +1,25 @@
+package com.nuriulgen.bitirmeodevi.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.nuriulgen.bitirmeodevi.R
+
+fun ImageView.showImage(imageUrl: String, placeholder: CircularProgressDrawable){
+    // Placeholder uyguladık.
+    val myOptions = RequestOptions().placeholder(placeholder).error(R.mipmap.ic_launcher)
+
+    // Glide extensions olarak oluşturduk.
+    Glide.with(context).setDefaultRequestOptions(myOptions).load(imageUrl).into(this)
+}
+
+fun showPlaceHolder(context: Context): CircularProgressDrawable{
+    // image yüklenene kadar image yerini almak için kullanıyoruz.
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}
