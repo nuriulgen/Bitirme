92fce1debf4a9fec26ef0516c7c91b9821f85847
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-10-09 16:25:33 +0300

    The bottom navigation bar is hidden when clicking on the detail page.

--- app/src/main/java/com/nuriulgen/bitirmeodevi/MainActivity.kt
+++ app/src/main/java/com/nuriulgen/bitirmeodevi/MainActivity.kt
@@ -2,13 +2,10 @@ package com.nuriulgen.bitirmeodevi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.get
-import androidx.core.view.isNotEmpty
+import android.view.View
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.nuriulgen.bitirmeodevi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,10 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        showBottomNavigation()
+        setupNavigation()
     }
 
-    private fun showBottomNavigation(){
+    private fun setupNavigation(){
         /**
          * Nav host Fragment ile Bottom Navigation Bar birbirine bağlandı.
          *
@@ -35,5 +32,27 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        /*
+         * Detay sayfasına tıklanıldığında bottom navigation bar gizlendi.
+         */
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> showBottomNav()
+                R.id.searchFragment -> showBottomNav()
+                R.id.tripFragment -> showBottomNav()
+                R.id.guideFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
     }
-}
+
+    private fun showBottomNav() {
+        binding.bottomNavigatinView.visibility = View.VISIBLE
+
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNavigatinView.visibility = View.GONE
+    }
+    }
